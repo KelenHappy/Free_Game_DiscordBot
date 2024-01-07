c0f8bb3b812e20ea335a4cc0e4d842ed40940529
Kelen <kelen940511@gmail.com>
2024-01-07 11:48:15 +0800

    update code

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from keep_alive import keep_alive
+import discord #導入discord
+import os
+
+bot = discord.Bot(intents = discord.Intents.all())
+#定義物件 intents是前面勾選的三個勾勾
+
+@bot.event
+async def on_ready():
+    print(f"「{bot.user}」已登入")
+
+keep_alive()
+bot.run(os.environ['bot_token']) #運行機器人
 import json
 import requests as req
 url = "https://www.4gamers.com.tw/site/api/news/by-tag?tag=%E9%99%90%E6%99%82%E5%85%8D%E8%B2%BB&nextStart=0&pageSize=25"
