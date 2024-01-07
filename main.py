3f0efb5c40b6c9b4228d53750180a0e592e4cc6b
Kelen <kelen940511@gmail.com>
2024-01-07 16:32:32 +0800

    update and create time

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# from keep_alive import keep_alive
-# import discord #導入discord
-# import os
+from keep_alive import keep_alive
+import discord #導入discord
+import os
 from getUrl import getUrl
 url = "https://www.4gamers.com.tw/site/api/news/by-tag?tag=%E9%99%90%E6%99%82%E5%85%8D%E8%B2%BB&nextStart=0&pageSize=25"
-# bot = discord.Bot(intents = discord.Intents.all())
-# #定義物件 intents是前面勾選的三個勾勾
-#
-# @bot.event
-# async def on_ready():
-#     print(f"「{bot.user}」已登入")
-# keep_alive()
+bot = discord.Bot(intents = discord.Intents.all())
+# 定義物件 intents是前面勾選的三個勾勾
+
+@bot.event
+async def on_ready():
+     print(f"「{bot.user}」已登入")
+keep_alive()
 getUrl(url)
-# bot.run(os.environ['bot_token'])
+bot.run(os.environ['bot_token'])
