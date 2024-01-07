bd30801811b6b20d5b1a2fe4476cd9ea126a2bbb
Kelen <kelen940511@gmail.com>
2024-01-07 14:11:23 +0800

    update

---
+++ getUrl.py
@@ -0,0 +1,16 @@
+import json
+from firebase import firebase
+import requests as req
+def getUrl(url):
+    firebase_url = 'https://freegamediscord-4d347-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    fdb = firebase.FirebaseApplication(firebase_url, None)
+    last_url = fdb.get('/','data')
+    res = req.get(url)
+    data = json.loads(res.text)
+    urlPush = data["data"]["results"][0]["canonicalUrl"]
+    counter = 0
+    while urlPush != last_url:
+        print(urlPush)
+        counter += 1
+        urlPush = data["data"]["results"][counter]["canonicalUrl"]
+    fdb.put('/','data',data["data"]["results"][0]["canonicalUrl"])
